1c9d08b5b2c5dfd1ea0a20de6c1ed5562bef531c
Robert Juliano <rovjuvano@gmail.com>
2015-09-25 16:07:11 -0400

    code club 2015-09-25

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,25 @@
 #![allow(dead_code)]
 struct VendingMachine {
-    is_alive: bool,
     coin: u32,
 }
 
 impl VendingMachine {
+    fn coin_return(&self)->Vec<u32>{
+        if self.coin > 0 {
+            vec![25,25]
+        }
+        else {
+            vec![1]
+        }
+    }
+
     fn new() -> Self {
         VendingMachine {
-            is_alive: false,
             coin: 0
         }
     }
     fn display(&self) -> String {
-        if self.is_alive {
+        if self.coin != 0 {
             format!("{:.2}", self.coin as f32 / 100.0)
         } else {
             "INSERT COIN".to_string()
@@ -20,9 +27,13 @@ impl VendingMachine {
     }
 
     fn insert_coin(&mut self, coin: u32) {
-        self.is_alive = true;
-        self.coin = coin;
+        if coin != 1 {
+            self.coin += coin;
+        }
     }
+
+    // fn press_coin_return(&mut self) {
+    // }
 }
 
 #[test]
@@ -46,6 +57,33 @@ fn it_accepts_dimes() {
 }
 
 #[test]
+fn it_accepts_quarters() {
+    let mut vm = VendingMachine::new();
+    vm.insert_coin(25);
+    assert_eq!(vm.display(), "0.25")
+}
+
+#[test]
+fn it_accepts_two_quarters() {
+    let mut vm = VendingMachine::new();
+    vm.insert_coin(25);
+    vm.insert_coin(25);
+    assert_eq!(vm.display(), "0.50")
+}
+
+#[test]
+fn it_should_return_coins_when_coin_return_is_pressed() {
+    let mut vm = VendingMachine::new();
+    vm.insert_coin(25);
+    vm.insert_coin(25);
+    // vm.press_coin_return();
+    assert_eq!(vm.coin_return(), vec![25,25])
+}
+
+#[test]
 fn it_does_not_accept_pennies() {
-    assert_eq!(true, true)
+    let mut vm = VendingMachine::new();
+    vm.insert_coin(1);
+    assert_eq!(vm.display(), "INSERT COIN");
+    assert_eq!(vm.coin_return(), vec![1]);
 }
