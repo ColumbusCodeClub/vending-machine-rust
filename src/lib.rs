1eb07881c76bd37a556b7d286fec0254769c2109
Robert Juliano <rovjuvano@gmail.com>
2015-10-02 16:08:03 -0400

    code club 2015-10-02

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,20 @@ mod test;
 
 struct VendingMachine {
     coin: u32,
+    coin_return: Vec<u32>,
+    coins: Vec<u32>,
 }
 
 impl VendingMachine {
-    fn coin_return(&self)->Vec<u32>{
-        if self.coin > 0 {
-            vec![25,25]
-        }
-        else {
-            vec![1]
-        }
+    fn coin_return(&self) -> Vec<u32> {
+        self.coin_return.clone()
     }
 
     fn new() -> Self {
         VendingMachine {
-            coin: 0
+            coin: 0,
+            coin_return: vec![],
+            coins: vec![],
         }
     }
     fn display(&self) -> String {
@@ -32,9 +31,19 @@ impl VendingMachine {
     fn insert_coin(&mut self, coin: u32) {
         if coin != 1 {
             self.coin += coin;
+            self.coins.push(coin)
+        }
+        else {
+            self.coin_return.push(coin)
         }
     }
 
-    // fn press_coin_return(&mut self) {
-    // }
+    fn press_coin_return(&mut self) {
+        if self.coin > 0 {
+            for coin in self.coins.clone().iter() {
+                self.coin_return.push(*coin);
+            }
+            self.coin = 0
+        }
+    }
 }
--- src/test.rs
+++ src/test.rs
@@ -38,10 +38,13 @@ fn it_accepts_two_quarters() {
 #[test]
 fn it_should_return_coins_when_coin_return_is_pressed() {
     let mut vm = VendingMachine::new();
+    vm.insert_coin(1);
     vm.insert_coin(25);
     vm.insert_coin(25);
-    // vm.press_coin_return();
-    assert_eq!(vm.coin_return(), vec![25,25])
+    assert_eq!(vm.coin_return(), vec![1]);
+    vm.press_coin_return();
+    assert_eq!(vm.coin_return(), vec![1, 25, 25]);
+    assert_eq!(vm.display(), "INSERT COIN");
 }
 
 #[test]
@@ -50,4 +53,14 @@ fn it_does_not_accept_pennies() {
     vm.insert_coin(1);
     assert_eq!(vm.display(), "INSERT COIN");
     assert_eq!(vm.coin_return(), vec![1]);
+}
+
+#[test]
+fn it_adds_a_nickle_and_dime_and_returns_them() {
+    let mut vm = VendingMachine::new();
+    vm.insert_coin(5);
+    vm.insert_coin(10);
+    assert_eq!(vm.display(), "0.15");
+    vm.press_coin_return();
+    assert_eq!(vm.coin_return(), vec![5, 10]);
 }
